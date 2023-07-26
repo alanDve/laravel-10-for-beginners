e044ce8b4dccf9bf45d6fdfff615f7ec3436587b
alanDve <115123399+alanDve@users.noreply.github.com>
2023-07-26 10:56:28 +0300

    lets bigen

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>I Love Home</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        h1 {
+            color: #2c3e50;
+            font-size: 48px;
+            text-shadow: 2px 2px 4px #ccc;
+            margin: 0;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+<body>
+    <h1>I Love Home</h1>
+</body>
+</html>
